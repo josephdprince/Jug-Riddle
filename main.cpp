60c6765e89e0654eef17e943c6006b91de0fc691
josephdprince <57415450+josephdprince@users.noreply.github.com>
2021-09-09 10:15:56 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,20 +23,5 @@ int main() {
    }
    cout << endl << solution << endl;
 
-   // string solution;
-   // Jug head1(3, 5, 4, 1, 2, 3, 4, 5, 6);
-   // if (head1.solve(solution) != 1) {
-   //    cout << "Error 3" << endl;
-   // }
-   // cout << solution << endl << endl;
-
-
-   // //string solution;
-   // Jug head2(3, 5, 4, 1, 1, 1, 1, 1, 2);
-   // if(head2.solve(solution) != 1) {
-   //    cout << "Error 3" << endl;
-   // }
-   // cout << solution << endl;
-
     return 0;
 }
